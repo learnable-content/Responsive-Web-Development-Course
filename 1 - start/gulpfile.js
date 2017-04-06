e58f0bda6702111e743ef6099b859315b4dbe74e
spxds <mactechy@gmail.com>
2017-04-05 22:56:40 -0400

    Add files via upload

---
+++ 1 - start/gulpfile.js
@@ -0,0 +1,6 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+var autoprefixer = require('gulp-autoprefixer');
+var imagemin = require('gulp-imagemin');
+var browserSync = require('browser-sync').create();
